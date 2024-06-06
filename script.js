221c4bf578fe27a15e84bf48866cdac4535fed40
D. Sukhdeo <144715223+DeveshSukhdeo@users.noreply.github.com>
2024-06-06 19:54:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function myfunction() {
+document.getElementById("greet").style.color="purple"
+}
+function buttontwo() {
+document.getElementById("sample").style.color="darkblue"
+}
+function wordscolor() {
+document.getElementById("paragraph").style.color="darkred"
+}
 
